893482c80883b7ddf0b0723468e3a5354e941cc7
zhujian <zhujian@ewell.cc>
2021-12-17 15:37:44 +0800

    [jdk1.8.0_261-源码初始提交] [带有测试案例]

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("1", "我");
+        System.out.println("我爱中国");
+    }
+
+}
